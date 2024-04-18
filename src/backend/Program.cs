5e8c118333590413fe8d3c75679720a687a208b5
plastic-plant <plastic-plant@patrick.nl>
2024-04-18 21:24:34 +0200

    How about some backend and frontend?

---
+++ src/backend/Program.cs
@@ -0,0 +1,10 @@
+namespace Howabout
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ src/tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Howabout.Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
